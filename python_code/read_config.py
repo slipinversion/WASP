1465186fd8429b3c5608b1c6309a0fe306054b87
pakk583 <66791409+pakk583@users.noreply.github.com>
2022-06-28 11:08:17 -0400

    Update read_config.py
    To simplify set up of code.

--- python_code/read_config.py
+++ python_code/read_config.py
@@ -1,8 +1,10 @@
 # stdlib imports
 import configparser
 import pathlib
+import os
 
-CONFIG_PATH = pathlib.Path.home() / 'production_code' / 'config.ini'
+folder = os.path.dirname(__file__)
+CONFIG_PATH = os.path.join(os.path.dirname(folder), 'config.ini')
 print(CONFIG_PATH)
 
 
@@ -19,8 +21,8 @@ if __name__ == '__main__':
     # which usefulness is described here:
     # https://www.enfoldsystems.com/software/proxy/docs/4.0/configuringmanually.html#the-default-section
     for section, options in config.items():
-        print(f'Options for section {section}:')
+        print('Options for section {section}:')
         for key, value in options.items():
-            print(f'\t{key} = {value}')
+            print(key, value)#'\t{key} = {value}')
     # grabbing values
     print(config['PATHS']['code_path'])
